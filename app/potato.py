1cb7a74be7ca1638dced8bd7dc1513d8131ecc10
georgezheng123 <gzheng3@stuy.edu>
2017-07-15 18:21:04 -0400

    priority schema

---
+++ app/potato.py
@@ -0,0 +1,35 @@
+from app import db
+from app import app
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(500))
+    titleSlug = db.Column(db.String(500))
+    content = db.Column(db.Text)
+    p_index = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime)
+    volume = db.Column(db.Integer)
+    issue = db.Column(db.Integer)
+
+    section_id = db.Column(db.Integer, db.ForeignKey('section.id'))
+    subsection_id = db.Column(db.Integer, db.ForeignKey('subsection.id'))
+
+
+class Section(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(500))
+    slug = db.Column(db.String(500))
+    description = db.Column(db.Text)
+
+    article_id = db.relationship('Article', backref='section', lazy='dynamic')
+
+class Subsection(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(500))
+    slug = db.Column(db.String(500))
+    description = db.Column(db.Text)    
+
+    article_id = db.relationship('Article', backref='subsection', lazy='dynamic')
+
