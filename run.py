c983c05f2513b9b44fbf2eedae7894145cf87de8
Nicholas Yang <ny585@nyu.edu>
2017-07-22 16:39:13 -0400

    Docker works now!

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app import app
 
 # This will run the application, change the debug do deliminate the nice error messages (every error would then result in an error 404 message)
 if __name__ == "__main__":
-    app.run(debug = True, use_reloader = False) # Can change back debug
+    app.run(debug = True, host='0.0.0.0', port=8000) # Can change back debug
