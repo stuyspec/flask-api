7927a16abf280667f66519e79d5a3135af0f56dd
georgezheng123 <gzheng3@stuy.edu>
2017-07-22 19:07:53 -0400

    seed data on run

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 #!flask/bin/python
 from app import app
 
+import db_seed
+
 # This will run the application, change the debug do deliminate the nice error messages (every error would then result in an error 404 message)
 if __name__ == "__main__":
     app.run(debug = True, host='0.0.0.0', port=8000) # Can change back debug
