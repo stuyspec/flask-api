0dc95f8466503620386f00131ff9b281557377eb
Nicholas Yang <ny585@nyu.edu>
2017-07-29 17:54:40 -0400

    Start of major refactor

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ from app import app, db, models
 #----------------------------------------------  Error Handlers
 
 @app.errorhandler(400)
-def not_found(error):
+def bad_request(error):
   return make_response(jsonify( { 'error': 'Bad request' } ), 400)
 
 @app.errorhandler(404)
@@ -16,10 +16,17 @@ def not_found(error):
 
 #---------------------------------------------- Section and Article Endpoints
 
+SECTION_ROUTE = "/sections/<string:section_slug>/" \
+                "subsection/<string:subsection_slug>"
 
-@app.route('/sections/<string:section_slug>/' + \
-           'subsection/<string:subsection_slug>' )
-def get_section_by_slug(section_slug,subsection_slug):
+SECTION_ARTICLES_ROUTE = "%s/articles" % (SECTION_ROUTE)
+
+SECTION_ARTICLE_ROUTE = "%s/<string:article_slug>" % (ARTICLES_ROUTE)
+
+ALL_ARTICLES_ROUTE = "/articles"
+
+@app.route(SECTION_ROUTE)
+def get_section_by_slug(section_slug, subsection_slug):
   if subsection_slug == "main":
     target = models.Section.query.filter(
       models.Section.slug == section_slug
@@ -29,14 +36,13 @@ def get_section_by_slug(section_slug,subsection_slug):
     target = models.Subsection.query.filter(
       models.Subsection.slug == subsection_slug
         ).first()
-    if target.parent_slug == section_slug: 
+    if target.parent_slug == section_slug:
       return jsonify({"description": target.description})
-article_route = '''
-/sections/<string:section_slug>/subsection/<string:subsection_slug>/articles/
-'''
-@app.route(article_route , defaults={'article_slug': None}) 
-@app.route(article_route + '<string:article_slug>' ) 
-def get_section_articles(section_slug,subsection_slug,article_slug):
+
+
+@app.route(SECTION_ARTICLES_ROUTE , defaults={'article_slug': None})
+@app.route(SECTION_ARTICLE_ROUTE)
+def get_section_articles(section_slug, subsection_slug, article_slug):
   if article_slug != None and article_slug != "None":
     articles = [models.Article.query.filter(
       models.Article.slug == article_slug
@@ -58,60 +64,62 @@ def get_section_articles(section_slug,subsection_slug,article_slug):
   secure_articles = []
   for article in articles:
     article_dict = {
-    "content": article.content, 
-    "datetime": article.datetime, 
-    "id": article.id, 
-    "is_draft": article.is_draft, 
-    "issue": article.issue, 
-    "section_id": article.section_id, 
-    "slug": article.slug, 
-    "subsection_id": article.subsection_id, 
-    "title": article.title, 
+    "content": article.content,
+    "datetime": article.datetime,
+    "id": article.id,
+    "is_draft": article.is_draft,
+    "issue": article.issue,
+    "section_id": article.section_id,
+    "slug": article.slug,
+    "subsection_id": article.subsection_id,
+    "title": article.title,
     "volume": article.volume
-          }   
+          }
     secure_articles.append(article_dict)
   return jsonify({"articles": secure_articles})
 
 @app.route('/newspaper/<int:volume>/<int:issue>' )
 def get_issue_articles(volume,issue):
-  articles = models.Article.query.filter(models.Article.volume == volume 
+  articles = models.Article.query.filter(models.Article.volume == volume
     and models.Article.issue == issue).all()
-  converted_articles = [] #Container for articles that has been converted to a dictionary to display data
+  # Container for articles that have
+  # been converted to a dictionary to display data
+  converted_articles = []
   for article in articles:
     article_dict = {
-    "content": article.content, 
-    "datetime": article.datetime, 
-    "id": article.id, 
-    "is_draft": article.is_draft, 
-    "issue": article.issue, 
-    "section_id": article.section_id, 
-    "slug": article.slug, 
-    "subsection_id": article.subsection_id, 
-    "title": article.title, 
-    "volume": article.volume
-          }   
+      "content": article.content,
+      "datetime": article.datetime,
+      "id": article.id,
+      "is_draft": article.is_draft,
+      "issue": article.issue,
+      "section_id": article.section_id,
+      "slug": article.slug,
+      "subsection_id": article.subsection_id,
+      "title": article.title,
+      "volume": article.volume
+    }
     converted_articles.append(article_dict)
-  issuu_code = models.Issuu.query.filter(models.Issuu.volume == volume 
+  issuu_code = models.Issuu.query.filter(models.Issuu.volume == volume
     and models.Issuu.issue == issue).first().code
   return jsonify({"issuu_code": issuu_code, "articles": secure_articles})
 
-@app.route('/list_articles/articles/' )
+@app.route(ALL_ARTICLES_ROUTE)
 def get_all_articles():
   articlesInIssue = models.Article.query.all()
   secure_articles = []
   for article in articlesInIssue:
     article_dict = {
-    "content": article.content, 
-    "datetime": article.datetime, 
-    "id": article.id, 
-    "is_draft": article.is_draft, 
-    "issue": article.issue, 
-    "section_id": article.section_id, 
-    "slug": article.slug, 
-    "subsection_id": article.subsection_id, 
-    "title": article.title, 
+    "content": article.content,
+    "datetime": article.datetime,
+    "id": article.id,
+    "is_draft": article.is_draft,
+    "issue": article.issue,
+    "section_id": article.section_id,
+    "slug": article.slug,
+    "subsection_id": article.subsection_id,
+    "title": article.title,
     "volume": article.volume
-          }   
+          }
     secure_articles.append(article_dict)
   limit = request.args.get('limit')
   if limit is not None:
@@ -123,11 +131,11 @@ def get_all_articles():
 def get_user(user_id):
   user =  models.User.query.get(user_id)
   user_data = {
-  "description": user.description, 
-  "email": user.email, 
-  "firstname": user.firstname, 
-  "id": user.id, 
-  "lastname": user.lastname, 
+  "description": user.description,
+  "email": user.email,
+  "firstname": user.firstname,
+  "id": user.id,
+  "lastname": user.lastname,
   "username": user.username
   }
   return jsonify( {"user_data": user_data} )
